3d579f55856c603c1aeb85f344ee558d392aac16
davidjevans <dezeumer@gmail.com>
2019-05-24 16:21:32 -0700

    adding installing of neobundle to linking process

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+sh ./install.sh
+rm ./install.sh
+
 cd ~/dot/files
 for file in $(find . -type f -o -type l | sed 's|./||'); do
     printf "${CYAN}Symlinking ${file} to ~/${file}\n"
