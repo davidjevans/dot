1774102fa14620568a6afbbd89ed639e79835a82
dave.evans <dave.evans@bluerivertech.com>
2023-06-14 15:37:23 -0700

    Can run setup.sh from anywhere

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-cd ~/dot/files
-for file in $(find . -type f -o -type l | sed 's|./||'); do
-	git_file_path=$( realpath ${file} )
-	printf "${CYAN}Symlinking ${file} to ~/${file}\n"
-    	ln -sf "$git_file_path" ~/"$file"
+dot_dir_location=$( dirname -- "$0")
+for file_name in $(find "${dot_dir_location}/files" -type f -o -type l); do
+	git_file_path=$( realpath ${file_name} )
+	printf "${CYAN}Symlinking ${git_file_path} to ~/${file_name}\n"
+	ln -sf "$git_file_path" ~/"$file"
 done
